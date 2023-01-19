f91c48bd88a3af241ba1aba40528246e2900afcc
Sidharth Deepesh <sidharthdeepesh@hotmail.com>
2023-01-19 22:30:33 +0530

    stage 3 task 1 done

--- stages/stage 3/exprtree.h
+++ stages/stage 3/exprtree.h
@@ -5,12 +5,15 @@ typedef struct tnode {
     int nodetype;   // information about non-leaf nodes - read/write/connector/+/* etc.
     struct tnode *left,*right;  // left and right branches
 }tnode;
-
 //create abstract syntax tree
-struct tnode* createTree(int val, char* varname, int nodetype, struct tnode* l, struct tnode* r);
-
+struct tnode* createTree(int val, int type, char* varname, int nodetype, struct tnode* l, struct tnode* r);
 /*To evaluate an expression tree*/
 void preorder(struct tnode* tnode);
+
+//expression types
+#define BOOLTYPE 0
+#define INTTYPE 1
+#define INVALIDTYPE -1
 
 //nodetype definition
 #define MATHOPNODE 1
@@ -20,6 +23,9 @@ void preorder(struct tnode* tnode);
 #define ASSIGNNODE 5
 #define VARNODE 6
 #define NUMNODE 7
+#define IFNODE 8
+#define WHILENODE 9
+#define LOGICOPNODE 10
 
 //generate code for the expression tree
 #define register_index int
